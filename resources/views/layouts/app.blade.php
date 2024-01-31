a99e66c1d7c1eec63343ef93945730dfdd7ecd33
Amin23ElKassimi <aminelkassimi01@gmail.com>
2024-01-31 21:16:54 +0100

    clean

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,8 +6,17 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>titolo</title>
     @vite('resources/js/app.js')
+    @yield('head-content')
 </head>
 <body>
+    @include('partials.header')
 
+    <main>
+        @yield('main-content')
+    </main>
+
+    @include('partials.footer')
+
+    @yield('script-content')
 </body>
 </html>
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,23 @@
+<header class="text-center">
+<div id="container">
+            <section class="logo">
+                <img src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt="Molisana main logo">
+            </section>
+            <nav >
+                <ul class="m-0">
+                    <li >
+                    comics 
+                    </li>
+                    <li >
+                    comics 
+                    </li>
+                    <li >
+                    comics 
+                    </li>
+                    <li >
+                    comics 
+                    </li>
+                </ul>
+            </nav>
+        </div>
+</header>
